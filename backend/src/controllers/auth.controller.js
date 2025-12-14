79a931b1648a27959345b4ee3c1c38bfc0866b95
Munyankindi François <147691671+Francis-250@users.noreply.github.com>
2025-12-14 17:25:42 +0200

    Update SameSite cookie attribute based on environment

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -85,7 +85,7 @@ export async function login(req, res) {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production"  ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).json({ message: "Login successful" });
